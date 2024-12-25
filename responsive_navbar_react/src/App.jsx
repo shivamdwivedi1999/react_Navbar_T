be043a19a493295536fe4818ed0fea180663d629
Dishank Singhal <dishanksinghal@Dishanks-MacBook-Pro.local>
2024-12-26 00:41:55 +0530

    first commit

---
+++ responsive_navbar_react/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import {Navbar} from "./components/Navbar";
+// import {HeroSection} from "./components/HeroSection"
+
+export const App =()=>{
+  return (
+    <>
+    <Navbar />
+    {/* <HeroSection/> */}
+    </>
+  )
+}
